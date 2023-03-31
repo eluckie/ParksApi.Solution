b36d0ca1b769f1e4bf416d13d8a5b50d87a53497
E Luckie <eluckie.d@gmail.com>
2023-03-31 11:22:56 -0600

    add model validation for Parks

--- ParksApi/Models/Park.cs
+++ ParksApi/Models/Park.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ParksApi.Models
 {
   public class Park
   {
     public int ParkId { get; set; }
+    [Required]
     public string Name { get; set; }
+    [Required]
     public string City { get; set; }
+    [Required]
+    [Range(4, 14, ErrorMessage = "Must be the full state name.")]
     public string State { get; set; }
   }
 }

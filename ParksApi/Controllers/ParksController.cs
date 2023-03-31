7aa115843f4eee5a9feabb53cb4c075ca37501de
E Luckie <eluckie.d@gmail.com>
2023-03-31 10:46:20 -0600

    add basic Get paths for full list & specific park

---
+++ ParksApi/Controllers/ParksController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParksApi.Models;
+
+namespace ParksApi.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class ParksController : ControllerBase
+  {
+    private readonly ParksApiContext _db;
+
+    public ParksController(ParksApiContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Park>>> Get()
+    {
+      return await _db.Parks.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Park>> GetPark(int id)
+    {
+      Park park = await _db.Parks.FindAsync(id);
+
+      if (park == null)
+      {
+        return NotFound();
+      }
+
+      return park;
+    }
+  }
+}

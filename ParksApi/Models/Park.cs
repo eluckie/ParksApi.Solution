f262daefca0a86374b50a75aadd1402b34a4f1e3
E Luckie <eluckie.d@gmail.com>
2023-03-31 12:23:40 -0600

    update Length requirements for state

--- ParksApi/Models/Park.cs
+++ ParksApi/Models/Park.cs
@@ -10,6 +10,7 @@ namespace ParksApi.Models
     [Required]
     public string City { get; set; }
     [Required]
+    [StringLength(15, MinimumLength = 4, ErrorMessage = "Please write out the entire state name.")]
     public string State { get; set; }
     public bool NationalPark { get; set; }
     public bool StatePark { get; set; } = false;

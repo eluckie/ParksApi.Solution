6c3e756fc06024f4880e54963f0933d3eb88fcb9
E Luckie <eluckie.d@gmail.com>
2023-03-31 10:38:25 -0600

    seed data to database for endpoint testing

--- ParksApi/Models/ParksApiContext.cs
+++ ParksApi/Models/ParksApiContext.cs
@@ -9,5 +9,17 @@ namespace ParksApi.Models
     public ParksApiContext(DbContextOptions<ParksApiContext> options) : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Park>()
+        .HasData(
+          new Park { ParkId = 1, Name = "Grand Canyon National Park", City = "Grand Canyon Village", State = "Arizona"},
+          new Park { ParkId = 2, Name = "Cherry Creek State Park", City = "Aurora", State = "Colorado"},
+          new Park { ParkId = 3, Name = "Chatfield State Park", City = "Littleton", State = "Colorado"},
+          new Park { ParkId = 4, Name = "Yosemite National Park", City = "Yosemite National Park", State = "California"},
+          new Park { ParkId = 5, Name = "Glacier National Park", City = "West Glacier", State = "Montana"}
+        );
+    }
   }
 }

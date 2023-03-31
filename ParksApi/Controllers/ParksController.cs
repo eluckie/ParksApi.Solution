3f935847aee5f82d535890141b4b905a2fc92778
E Luckie <eluckie.d@gmail.com>
2023-03-31 11:00:59 -0600

    add Put functionality to parks

--- ParksApi/Controllers/ParksController.cs
+++ ParksApi/Controllers/ParksController.cs
@@ -44,5 +44,40 @@ namespace ParksApi.Controllers
       await _db.SaveChangesAsync();
       return CreatedAtAction(nameof(GetPark), new { id = park.ParkId }, park);
     }
+
+    // PUT parks/id
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Park park)
+    {
+      if (id != park.ParkId)
+      {
+        return BadRequest();
+      }
+
+      _db.Parks.Update(park);
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!ParkExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return Accepted();
+    }
+
+    private bool ParkExists(int id)
+    {
+      return _db.Parks.Any(entry => entry.ParkId == id);
+    }
   }
 }

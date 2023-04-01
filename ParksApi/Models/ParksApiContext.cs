dcf142397bd4ce30ede0b68d24afdc0be4ec74d5
E Luckie <eluckie.d@gmail.com>
2023-04-01 11:08:05 -0600

    add Users to database

--- ParksApi/Models/ParksApiContext.cs
+++ ParksApi/Models/ParksApiContext.cs
@@ -5,6 +5,7 @@ namespace ParksApi.Models
   public class ParksApiContext : DbContext
   {
     public DbSet<Park> Parks { get; set; }
+    public DbSet<User> Users { get; set; }
 
     public ParksApiContext(DbContextOptions<ParksApiContext> options) : base(options)
     {
